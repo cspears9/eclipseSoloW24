7394ed0fb20846170071fd462c3cb8470fc89514
cspears8 <connorxspears@gmail.com>
2024-02-12 10:52:58 -0500

    Third edit to line 6

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy y'all!");
-		System.out.println("I wish I wasn't in Eclipse! :^) Alternate");
+		System.out.println("I wish I wasn't in Eclipse! :^) Alternatively I would like to use IntelliJ");
 	}
 
 }
